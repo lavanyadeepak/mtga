c4a6c8147a1934e57571ba0787e77676ece401c8
Deepak Kumar Vasudevan <deepak.vasudevan@outlook.com>
2026-03-25 22:18:45 +0530

    corrections

---
+++ assets/scripts/config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+  "city": "Tambaram",
+  "corporation": "Tambaram City Municipal Corporation",
+  "slogan": "Make Tambaram Great Again",
+  "hashtags": ["#MTGA", "#TambaramCorp", "#CitizenInitiative"],
+  "languages": ["en", "ta"],
+  "mapsApiKey": "YOUR_KEY_HERE",
+  "defaultLat": 12.9247660,
+  "defaultLng": 80.1515043,
+  "mapSvg": "assets/database/maps/tbm_map.svg",
+  "mapPdf": "assets/database/maps/tbm_map.pdf",
+  "zones": [],
+  "wards": [],
+  "socialTags": ["@TambaramCorp", "@CMOTamilNadu"],
+  "viralTags": ["@realDonaldTrump"] // 😄 optional but encouraged
+};
